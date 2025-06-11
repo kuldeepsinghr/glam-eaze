3cb0985bb7d4966b813afd92faa8d6f7627e4514
kuldeepsinghr <91716149+kuldeepsinghr@users.noreply.github.com>
2025-06-11 12:29:32 +0530

    Test lefthook formatting

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
-
-
 export default function Home() {
+  console.log('hel');
+
+  const wate = 'd';
+
   return (
-    <div>Hy</div>
+    <div>
+      <h1>hy</h1>
+
+      <h2>hello</h2>
+    </div>
   );
 }
